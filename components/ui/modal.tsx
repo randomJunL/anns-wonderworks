cee4aabe26dd1f55b98a5f6013951c3429126ff8
junlyu <jlyu47@gatech.edu>
2026-01-07 22:43:21 -0700

    install components

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import React from "react";
-import { Dialog, DialogContent, DialogHeader, DialogDescription, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogDescription, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 interface ModalProps {
     title: String;
