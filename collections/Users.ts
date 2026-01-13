a5d4882e5f379b9a942c4f4735115491231037b5
junlyu <jlyu47@gatech.edu>
2026-01-13 12:47:02 -0700

    install payload

---
+++ collections/Users.ts
@@ -0,0 +1,26 @@
+import type { CollectionConfig } from 'payload'
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: true,
+  fields: [
+    {
+      name: 'email',
+      type: 'email',
+      required: true,
+    },
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'age',
+      type: 'number',
+      required: false,
+    },
+  ],
+}
